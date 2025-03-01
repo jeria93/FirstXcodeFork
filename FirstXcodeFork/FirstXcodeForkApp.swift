c6ac51a4ec0402433a1854ac28bc6aff2f569e8d
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-03-01 23:17:57 +0100

    Initial Commit

---
+++ FirstXcodeFork/FirstXcodeForkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstXcodeForkApp.swift
+//  FirstXcodeFork
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-03-01.
+//
+
+import SwiftUI
+
+@main
+struct FirstXcodeForkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
